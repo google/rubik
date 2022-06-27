7ebdd66f87878445a6c2cc1959268439f3b5d84f
Roberto Santos <robsantos@google.com>
2022-06-27 10:14:00 -0300

    Cleaning project, adding license on SQL

--- sql/rubik_view.sql
+++ sql/rubik_view.sql
@@ -1,10 +1,24 @@
--- Template for creating the required view for Rubik on BigQuery.
+-- 
+-- Copyright 2022 Google Inc.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at 
+-- 
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
 
-CREATE OR REPLACE VIEW `megalista-robsantos-340012.rubik_mc_test.rubik_view` AS (
+
+CREATE OR REPLACE VIEW `project.dataset.tablename` AS (
     WITH
         ProductTable AS (
             SELECT *, DATE(_PARTITIONTIME) AS `date`
-            FROM `megalista-robsantos-340012.rubik_mc_test.Products_*`
+            FROM `project.dataset.Products_*`
         ),
         TodayProducts AS (
             SELECT *
